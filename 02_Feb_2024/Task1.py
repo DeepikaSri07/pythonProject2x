93414727b16491723488b8bc28e7442dbd6e763b
Deepika <contactdeepikasrivastava@gmail.com>
2024-02-07 15:25:02 +0800

    Task1: print user details

---
+++ 02_Feb_2024/Task1.py
@@ -0,0 +1,9 @@
+# Task1-Take a user input (name, age, roll_number, phone_number)and print the user details
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+roll_number = input("Enter Roll number: ")
+phone_number = input("Enter Phone number: ")
+print(name)
+print(age)
+print(roll_number)
+print(phone_number)
