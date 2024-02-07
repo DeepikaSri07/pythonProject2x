b8578bbf2cc11e84d84f47338ed6ef37852d9f0b
Deepika <contactdeepikasrivastava@gmail.com>
2024-02-07 15:27:58 +0800

    Task2: print table of 2

---
+++ 02_Feb_2024/Task2.py
@@ -0,0 +1,22 @@
+#Task2: Print the table of 2
+print("Table of 2 is")
+a=2*1
+b=2*2
+c=2*3
+d=2*4
+e=2*5
+f=2*6
+g=2*7
+h=2*8
+i=2*9
+j=2*10
+print("2*1=",a)
+print("2*2=",b)
+print("2*3=",c)
+print("2*4=",d)
+print("2*5=",e)
+print("2*6=",f)
+print("2*7=",g)
+print("2*8=",h)
+print("2*9=",i)
+print("2*10=",j)
