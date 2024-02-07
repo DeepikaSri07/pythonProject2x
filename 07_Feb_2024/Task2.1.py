a9f19c21cb2a59f869f58309b86011681c21939f
Deepika <contactdeepikasrivastava@gmail.com>
2024-02-07 17:53:59 +0800

    Task2.1 Calculate area of a circle.

---
+++ 07_Feb_2024/Task2.1.py
@@ -0,0 +1,5 @@
+# Write a Python program to calculate the area of a circle given its radius using the formula area=π×r^2 ( Take pie
+# as 3.14)
+r = float(input("Enter the radius of a circle "))
+Area = 3.14*r*r
+print("Area of the circle is",Area)
