407eacbc8d64ed9ebb3b03b8d35a935002b5fa5d
Deepika <contactdeepikasrivastava@gmail.com>
2024-02-07 17:45:24 +0800

    Task2.3 print square & cube of given number.

---
+++ 07_Feb_2024/Task2.3.py
@@ -0,0 +1,6 @@
+# Develop a Python script that calculates the square and cube of a given number.
+x = float(input("Enter the number "))
+a = (x * x)
+print("Square of the given number is", a)
+b = (x * x * x)
+print("Cube of the given number is", b)
