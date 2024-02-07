2ddd92bca5c182c2a6079a5f50ee61c4b5080f38
Deepika <contactdeepikasrivastava@gmail.com>
2024-02-07 16:57:39 +0800

    Task2.2 check 1st no is greater, less or equals to 2nd no.

---
+++ 07_Feb_2024/Task2.2.py
@@ -0,0 +1,11 @@
+# Create a program that takes two numbers as input and prints whether the
+# first number is greater than, less than, or equal to the second number.
+print("Check the first number is greater than, less than or equals to second number")
+num1 = float(input("Enter First number:"))
+num2 = float(input("Enter Second number:"))
+if num1 > num2:
+ print("First number is greater than Second number")
+if num1 < num2:
+ print("First number is less than Second number")
+if num1 == num2:
+ print("First number is equals to Second number")
